04189d217f2eb768c18f39e1ccc88cbb181dfe33
rugue <nehirugue@gmail.com>
2021-10-01 10:30:06 +0100

    Add completed challenge

--- PlayFetch/Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ PlayFetch/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -16,17 +16,19 @@ public class SpawnManagerX : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
+        Invoke("SpawnRandomBall", startDelay);
     }
 
     // Spawn random ball at random x position at top of play area
     void SpawnRandomBall ()
     {
+        Invoke("SpawnRandomBall", Random.Range(3, 10));
         // Generate random ball index and random spawn position
         Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
 
         // instantiate ball at random spawn location
-        Instantiate(ballPrefabs[0], spawnPos, ballPrefabs[0].transform.rotation);
+        //Instantiate(ballPrefabs[0], spawnPos, ballPrefabs[0].transform.rotation);
+        Instantiate(ballPrefabs[Random.Range(0, 3)], spawnPos, ballPrefabs[Random.Range(0, 3)].transform.rotation);
     }
 
 }
